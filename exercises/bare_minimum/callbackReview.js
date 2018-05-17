9b36fa5ecf89e566e445caf0d24c594aac9aaf4d
enague <ennague@gmail.com>
2018-05-17 16:27:26 -0700

    promise progress

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -4,15 +4,29 @@
 
 var fs = require('fs');
 var request = require('request');
+var https = require('https');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+  fs.readFile(filePath, 'utf8', function (err, content) {
+    if (err) {
+      callback(err);
+    } else {
+      callback(err, content.split('\n')[0]);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, callback) {
+
+  request(url, function(err, response, body) {
+    if(err) {
+      callback(err);
+    } else {
+      callback(err, response.statusCode);
+    }
+  })
 };
 
 // Export these functions so we can test them and reuse them in later exercises
